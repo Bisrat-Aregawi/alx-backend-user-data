c623db89b4fad6ec35a6b432ce4c69ac429f16b9
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-12 15:45:46 +0300

    Extend `format` method of logging.Formatter
    By using `filter_datum` obfuscator, extend `format` method of
    logging.Formatter class to redact sensitive fields in a LogRecord.
    
    For task 1 of Solo-Project: - `0x00-personal_data`

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,28 @@
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Extended format function from parent class"""
+        return filter_datum(
+            self.fields, self.REDACTION,
+            logging.Formatter.format(self, record), self.SEPARATOR,
+        )
 
 
 def filter_datum(
