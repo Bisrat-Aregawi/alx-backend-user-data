5478f270aebf310a447468da12d5dfbc359e2a2e
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-16 15:27:20 +0300

    Add error handler and an endpoint for unauthorized
    For task 1 of Solo-Project: - `0x01-Basic_authentication`

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """ Raise 401 error on request
+    """
+    abort(401)
