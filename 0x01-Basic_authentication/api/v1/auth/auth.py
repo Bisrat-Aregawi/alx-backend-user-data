4c3d9fc8c61d43bb6ddd5906d78d8ea078f2ea10
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-16 16:47:03 +0300

    Update `require_path` method of class `Auth`
    Return true for paths not excluded. True meaning there is a need for
    authentication
    
    For task 4 of Solo-Project: - `0x01-Basic_authentication`

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,12 @@ class Auth():
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """`require_auth` function"""
-        return False
+        if path:
+            if excluded_paths is not None and excluded_paths != []:
+                if path in excluded_paths or path + '/' in excluded_paths:
+                    return False
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """`authorization_header` function"""
