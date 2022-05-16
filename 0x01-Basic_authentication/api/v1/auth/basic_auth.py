198e0ec0449bbaed1dc8417dfff9fd8a29291c27
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-16 23:28:35 +0300

    Implement a base64 auth extractor method of `BasicAuth` class
    For task 7 of Solo-Project: - `0x01-Basic_authentication`

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 """Module defines `BasicAuth` class"""
+from typing import Union
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """BasicAuth class
     """
+
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> Union[str, None]:
+        """Return encoded part of `Authorization` header if any
+
+        Args:
+            authorization_header: Authorization header of request passed
+
+        Returns:
+            encoded part, None if passed header is not a string
+        """
+        if authorization_header and isinstance(authorization_header, str):
+            auth_header = authorization_header.split(' ')
+            if auth_header[0] == "Basic":
+                return auth_header[1]
+            return None
+        return None
     pass
