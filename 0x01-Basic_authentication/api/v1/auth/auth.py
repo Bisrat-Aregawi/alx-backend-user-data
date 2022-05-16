287366cd546a98cf2a0404721b499847b8e69d32
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-16 22:23:35 +0300

    Add a function which filters paths using Auth methods
    Abort with 403 if current user is not detected
    Abort with 401 if requested path is not excluded from authorization
    
    For task 5 of Solo-Project: - `0x01-Basic_authentication`

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
-"""Module defines `auth` class"""
-from flask import request
-from typing import List, TypeVar
+"""Module defines `Auth` class"""
+from typing import List, TypeVar, Union
 
 
 class Auth():
-    """Auth class"""
+    """Auth class
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """`require_auth` function"""
+        """Function checks request paths against excluded ones
+
+        Args:
+            path: path requested
+            excluded_paths: list of paths internally deemed public
+
+        Returns:
+            True if path is not found in excluded_paths, False otheriwse
+        """
         if path:
             if excluded_paths is not None and excluded_paths != []:
                 if path in excluded_paths or path + '/' in excluded_paths:
                     return False
         return True
 
+    def authorization_header(self, request=None) -> Union[str, None]:
+        """Function returns value of the authorization header `Authorization
 
-    def authorization_header(self, request=None) -> str:
-        """`authorization_header` function"""
-        return None
+        Args:
+            request: request object (imported from flask & passed by reference)
+
+        Returns:
+            Value of Authorization header if any, None if request is None
+        """
+        if not request:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """`current_user` function"""
