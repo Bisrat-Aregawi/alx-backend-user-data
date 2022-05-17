7b681c86363c94e2293f8d352dca588d36258798
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-17 11:28:20 +0300

    Improve Auth's `require_auth` method to allow `*` matching
    '*' meaning any number of characters can replace it
    
    For task 13 of Solo-Project: - `0x01-Basic_authentication`

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module defines `Auth` class"""
+import re
 from typing import List, TypeVar, Union
 
 
@@ -21,6 +22,9 @@ class Auth():
             if excluded_paths is not None and excluded_paths != []:
                 if path in excluded_paths or path + '/' in excluded_paths:
                     return False
+                for pth in excluded_paths:
+                    if pth[-1] == '*' and re.compile(pth[:-1]).match(path):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> Union[str, None]:
