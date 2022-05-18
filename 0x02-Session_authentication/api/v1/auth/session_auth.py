89ac680f2440d1d0d5a6c99e6c9aac4264c3032a
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-18 10:50:32 +0300

    Add `create_session` method to `SessionAuth` class
    Create an in memory session storage which is a class attribute of
    SesisonAuth, and use create_session method to assign new session ids to
    current_user
    
    For task 2 of Solo-Project: - `0x02-Session_authentication`

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """Module defines `SessionAuth` class"""
+from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """SessionAuth class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session id for user with id `user_id`
+
+        Args:
+            user_id: id attrib of a `User` instance
+
+        Returns:
+            new uuid4 session id, None if user_id is none or not a string
+        """
+        if user_id:
+            if isinstance(user_id, str):
+                sessid = str(uuid4())
+                self.__class__.user_id_by_session_id[sessid] = user_id
+                return sessid
+        return None
     pass
