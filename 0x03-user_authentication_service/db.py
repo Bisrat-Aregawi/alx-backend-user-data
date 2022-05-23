7025a68f880be52e6e528877dfe9af271c66d4ca
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-23 13:35:25 +0300

    Implement `find_user_by` method in `DB` class
    Raise `NoResultFound` error when no record was returned, and allow for
    `InvalidRequestError` to be raised for when Column name couldn't be
    satisfied by any key of kwargs.
    
    For task 2 of Solo-Project: - `0x03-user_authentication_service`

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
@@ -15,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -46,3 +47,27 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """Fetch 'a' user from `users` table
+
+        Return the first matched user from `users` table that satisfies
+        `kwargs` criteria. Keys of `kwargs` are search terms for table columns,
+        and values will be record values at those columns in `users` table.
+
+        Args:
+            kwargs: a dictionary used as record and field identifier when
+            querying
+
+        Returns:
+            a `users` table record which matches all criteria (`kwargs`)
+
+        Raises:
+            NoResultFound: raised when nothing was found in `users` table
+            InvalidRequestError: raised when kwargs key doesn't resemble any
+            column in `users` table
+        """
+        usr = self._session.query(User).filter_by(**kwargs).first()
+        if usr:
+            return usr
+        raise NoResultFound
