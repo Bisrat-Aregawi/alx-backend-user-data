8ea25f44d078b9f9edcfeba0a6bafdc025dc5180
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-23 10:25:06 +0300

    Create a `users` table model class `User`
    For task 0 of Solo-Project: - `0x03-user_authentication_service`

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module defines `User` class"""
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """`User` class that maps `users` table in DB
+
+    Attributes:
+        __tablename__: name of table in the database
+        id: id column of table
+        email: email column of table
+        hashed_password: email column of table
+        session_id: email column of table
+        reset_token: email column of table
+    """
+    __tablename__ = "users"
+    id: Column = Column(Integer, primary_key=True)
+    email: Column = Column(String(250), nullable=False)
+    hashed_password: Column = Column(String(250), nullable=False)
+    session_id: Column = Column(String(250))
+    reset_token: Column = Column(String(250))
