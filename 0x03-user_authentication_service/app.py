e5d4f6de5bf0b4aebb171c51db025cff1d6e281e
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-24 10:14:09 +0300

    Prepare a logout route for `/sessions` route
    For task 14 of Solo-Project: - `0x03-user_authentication_service`

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,9 @@
 """Module defines a flask application"""
 from typing import Tuple
 from flask.wrappers import Response
+from werkzeug.wrappers import Response
 from auth import Auth
-from flask import abort, Flask, request
+from flask import abort, Flask, redirect, request 
 from flask.json import jsonify
 
 
@@ -63,6 +64,18 @@ def login() -> Tuple[Response, int]:
             out.set_cookie("session_id", cookie)
             return (out, 200)
     abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout() -> Response:
+    """Destory the current user session and redirect to home"""
+    sessid = request.cookies.get("session_id")
+    if sessid:
+        usr = AUTH._db.find_user_by(session_id=sessid)
+        if usr:
+            AUTH.destroy_session(int(usr.id))
+            return redirect("/")
+    abort(403)
 
 
 if __name__ == "__main__":
