eb3c4278d29f2892fdb41179e325a7280b1a43e5
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-25 08:50:14 +0300

    Prepare "/reset_password" route for reset token creation
    For task 17 of Solo-Project: - `0x03-user_authentication_service`

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,5 +96,21 @@ def profile() -> Tuple[Response, int]:
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token() -> Response:
+    """Respond with a reset token for a valid email"""
+    email = request.form.get("email", "")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify(
+            {
+                "email": "{}".format(email),
+                "reset_token": "{}".format(reset_token)
+            }
+        )
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
