c8fdcdd6593473ce7a2b1a1f7687d09e3dfa5efe
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-25 07:53:18 +0300

    Checker fixes for task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,74 +32,8 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(
-                email, _hash_password(password)
+                email, _hash_password(password).decode("utf-8")
             )
-
-    def valid_login(self, email: str, password: str) -> bool:
-        """Validate a login request
-
-        Args:
-            email: email of user
-            password: password of user
-
-        Returns:
-            True if email and password are in database, False otherwise
-        """
-        try:
-            usr = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(
-                password.encode("utf-8"),
-                usr.hashed_password
-            )
-        except NoResultFound:
-            return False
-
-    def create_session(self, email: str) -> str:
-        """Assign a and return new session id to user
-
-        Find a user with email=`email` and assign it a session id using
-        `_generate_uuid` function.
-
-        Args:
-            email: email of user
-
-        Returns:
-            New session id created for user with email `email`
-        """
-        try:
-            usr = self._db.find_user_by(email=email)
-            sessid = _generate_uuid()
-            self._db.update_user(usr.id, session_id=sessid)
-            return sessid
-        except NoResultFound:
-            pass
-
-    def get_user_from_session_id(self, session_id: str) -> User | None:
-        """Retrieve a user record by session id from database
-
-        Args:
-            session_id: stored session cookie
-
-        Returns:
-            Corresponding user if found, None otherwise
-        """
-        try:
-            usr = self._db.find_user_by(session_id=session_id)
-            return usr
-        except NoResultFound:
-            return None
-
-    def destroy_session(self, user_id: int) -> None:
-        """Update corresponding user's session ID to None
-
-        Args:
-            user_id: user id
-
-        Returns:
-            None
-        """
-        self._db.update_user(user_id, session_id=None)
-        return None
     pass
 
 
